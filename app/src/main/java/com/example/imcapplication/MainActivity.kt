40afb5ac945616766bed71721620b1c78a088ecf
Marcus Lira <marcuslira57@gmail.com>
2021-09-16 15:39:52 -0300

    Final commit

--- app/src/main/java/com/example/imcapplication/MainActivity.kt
+++ app/src/main/java/com/example/imcapplication/MainActivity.kt
@@ -1,58 +1,38 @@
 package com.example.imcapplication
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.imcapplication.databinding.ActivityMainBinding
+import androidx.core.widget.doOnTextChanged
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    private fun setListeners(){
+        val alturaEdit: EditText = findViewById(R.id.altura)
+        val pesoEdit: EditText = findViewById(R.id.peso)
+        val buttonCalc : Button = findViewById(R.id.calcular)
+        buttonCalc.setOnClickListener{calc(pesoEdit.text.toString(), alturaEdit.text.toString())}
+    }
 
-        setSupportActionBar(binding.toolbar)
+    private fun calc(peso: String, altura: String){
+        val resultText: TextView = findViewById(R.id.titleText)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso !=null && altura != null){
+            val imc = peso /(altura*altura)
+            resultText.text = "Seu IMC é:${imc}"
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
